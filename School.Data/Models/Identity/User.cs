a1427adc5a8217031c5fe6600ec2e764cd5fa0f9
Ahmed hamza <ah2700749@gmail.com>
2025-02-09 05:39:57 +0200

    add identity tables

--- School.Data/Models/Identity/User.cs
+++ School.Data/Models/Identity/User.cs
@@ -2,7 +2,7 @@
 
 namespace School.Data.Models.Identity
 {
-    public class User : IdentityUser<int>
+    public class user : IdentityUser<int>
     {
         public string Address { get; set; }
     }
--- School.Infrastracture/Data/AppDbContext.cs
+++ School.Infrastracture/Data/AppDbContext.cs
@@ -7,10 +7,10 @@ using System.Reflection;
 
 namespace School.Infrastracture.Data
 {
-    public class AppDbContext : IdentityDbContext<User, IdentityRole<int>, int, IdentityUserClaim<int>, IdentityUserRole<int>, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
+    public class AppDbContext : IdentityDbContext<user, IdentityRole<int>, int, IdentityUserClaim<int>, IdentityUserRole<int>, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
+        public DbSet<user> Users { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Subject> Subjects { get; set; }
